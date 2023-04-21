537a6e70b996a17096adac75b464056b71d1fcd0
ryandaryl <ryandarylmills@gmail.com>
2023-04-21 20:38:02 +1000

    Pass tensors as separate variables

--- label_propagation/label_propagation.py
+++ label_propagation/label_propagation.py
@@ -10,6 +10,9 @@ import torch_geometric.transforms as T
 from torch_geometric.nn import LabelPropagation
 
 
+model = LabelPropagation(num_layers=3, alpha=0.9)
+
+
 root = osp.join('data', 'OGB')
 dataset = PygNodePropPredDataset(
     'ogbn-arxiv', root, transform=T.Compose([
@@ -18,12 +21,12 @@ dataset = PygNodePropPredDataset(
     ]))
 split_idx = dataset.get_idx_split()
 data = dataset[0]
+y = data.y
+adj_t = data.adj_t
 
-model = LabelPropagation(num_layers=3, alpha=0.9)
 
-
-out = model(data.y, data.adj_t, mask=split_idx['train'])
+out = model(y, adj_t, mask=split_idx['train'])
 y_pred = out.argmax(dim=-1, keepdim=True)
 for k in ['valid', 'test']:
-    print(k, (data.y[split_idx[k]] == y_pred[split_idx[k]]).sum() /
+    print(k, (y[split_idx[k]] == y_pred[split_idx[k]]).sum() /
           split_idx[k].shape[0])
