13926a6048cd2720ca9c4731da241d994ac6ef85
ryandaryl <ryandarylmills@gmail.com>
2023-05-11 06:02:14 +0000

    Move GCN model back to models script

--- gat/gat.py
+++ gat/gat.py
@@ -2,91 +2,11 @@ import math
 
 import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import dgl.nn.pytorch as dglnn
 from ogb.nodeproppred import DglNodePropPredDataset, Evaluator
 import pytorch_lightning as pl
 
-from models import GAT
-
-
-class GCN(pl.LightningModule):
-    def __init__(
-        self,
-        in_feats,
-        n_hidden,
-        n_classes,
-        n_layers,
-        activation,
-        dropout,
-        use_linear,
-        val_metric=None,
-    ):
-        super().__init__()
-        self.n_layers = n_layers
-        self.n_hidden = n_hidden
-        self.n_classes = n_classes
-        self.use_linear = use_linear
-        self.val_metric = val_metric
-
-        self.convs = nn.ModuleList()
-        if use_linear:
-            self.linear = nn.ModuleList()
-        self.bns = nn.ModuleList()
-
-        for i in range(n_layers):
-            in_hidden = n_hidden if i > 0 else in_feats
-            out_hidden = n_hidden if i < n_layers - 1 else n_classes
-            bias = i == n_layers - 1
-
-            self.convs.append(dglnn.GraphConv(in_hidden, out_hidden, "both", bias=bias))
-            if use_linear:
-                self.linear.append(nn.Linear(in_hidden, out_hidden, bias=False))
-            if i < n_layers - 1:
-                self.bns.append(nn.BatchNorm1d(out_hidden))
-
-        self.dropout0 = nn.Dropout(min(0.1, dropout))
-        self.dropout = nn.Dropout(dropout)
-        self.activation = activation
-
-    def forward(self, graph, feat):
-        h = feat
-        h = self.dropout0(h)
-
-        for i in range(self.n_layers):
-            conv = self.convs[i](graph, h)
-
-            if self.use_linear:
-                linear = self.linear[i](h)
-                h = conv + linear
-            else:
-                h = conv
-
-            if i < self.n_layers - 1:
-                h = self.bns[i](h)
-                h = self.activation(h)
-                h = self.dropout(h)
-
-        return h
-
-    def configure_optimizers(self):
-        return optim.RMSprop(self.parameters(), lr=0.002, weight_decay=0)
-
-    def training_step(self, batch, batch_idx):
-        graph, feat, labels, idx = batch[0]
-        pred = self(graph, feat)
-        loss = cross_entropy(pred[idx], labels[idx])
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        graph, feat, labels, idx = batch[0]
-        pred = self(graph, feat)
-        if self.val_metric:
-            self.log_dict(self.val_metric(pred, labels, idx), prog_bar=True)
-        loss = cross_entropy(pred[idx], labels[idx])
-        return loss
+from models import GCN, GAT
 
 
 class DataModule(pl.LightningDataModule):
@@ -180,7 +100,7 @@ def main():
         for stage in ["train", "val", "test"]
     }
 
-    """model = GCN(
+    model = GCN(
         in_feats=in_feats + n_classes,
         n_classes=n_classes,
         n_hidden=n_hidden,
@@ -189,8 +109,8 @@ def main():
         dropout=dropout,
         use_linear=False,
         val_metric=accuracy,
-    )"""
-    model = GAT(
+    )
+    """model = GAT(
         in_feats + n_classes,
         n_classes,
         n_hidden=n_hidden,
@@ -201,7 +121,7 @@ def main():
         attn_drop=attn_drop,
         norm=norm,
         val_metric=accuracy,
-    )
+    )"""
     print([np.prod(p.size()) for p in model.parameters() if p.requires_grad])
     print(sum([np.prod(p.size()) for p in model.parameters() if p.requires_grad]))
 
--- gat/models.py
+++ gat/models.py
@@ -12,6 +12,83 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 
+class GCN(pl.LightningModule):
+    def __init__(
+        self,
+        in_feats,
+        n_hidden,
+        n_classes,
+        n_layers,
+        activation,
+        dropout,
+        use_linear,
+        val_metric=None,
+    ):
+        super().__init__()
+        self.n_layers = n_layers
+        self.n_hidden = n_hidden
+        self.n_classes = n_classes
+        self.use_linear = use_linear
+        self.val_metric = val_metric
+
+        self.convs = nn.ModuleList()
+        if use_linear:
+            self.linear = nn.ModuleList()
+        self.bns = nn.ModuleList()
+
+        for i in range(n_layers):
+            in_hidden = n_hidden if i > 0 else in_feats
+            out_hidden = n_hidden if i < n_layers - 1 else n_classes
+            bias = i == n_layers - 1
+
+            self.convs.append(dglnn.GraphConv(in_hidden, out_hidden, "both", bias=bias))
+            if use_linear:
+                self.linear.append(nn.Linear(in_hidden, out_hidden, bias=False))
+            if i < n_layers - 1:
+                self.bns.append(nn.BatchNorm1d(out_hidden))
+
+        self.dropout0 = nn.Dropout(min(0.1, dropout))
+        self.dropout = nn.Dropout(dropout)
+        self.activation = activation
+
+    def forward(self, graph, feat):
+        h = feat
+        h = self.dropout0(h)
+
+        for i in range(self.n_layers):
+            conv = self.convs[i](graph, h)
+
+            if self.use_linear:
+                linear = self.linear[i](h)
+                h = conv + linear
+            else:
+                h = conv
+
+            if i < self.n_layers - 1:
+                h = self.bns[i](h)
+                h = self.activation(h)
+                h = self.dropout(h)
+
+        return h
+
+    def configure_optimizers(self):
+        return optim.RMSprop(self.parameters(), lr=0.002, weight_decay=0)
+
+    def training_step(self, batch, batch_idx):
+        graph, feat, labels, idx = batch[0]
+        pred = self(graph, feat)
+        loss = cross_entropy(pred[idx], labels[idx])
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        graph, feat, labels, idx = batch[0]
+        pred = self(graph, feat)
+        if self.val_metric:
+            self.log_dict(self.val_metric(pred, labels, idx), prog_bar=True)
+        loss = cross_entropy(pred[idx], labels[idx])
+        return loss
+
+
 class Bias(nn.Module):
     def __init__(self, size):
         super().__init__()
